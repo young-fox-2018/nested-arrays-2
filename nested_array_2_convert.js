cf8242088d44e01e0fe0ee86a64fae7d50ea071d
Cascade <mufhabu@gmail.com>
2018-10-31 15:04:24 +0700

    done-0nested-array 2

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -2,17 +2,29 @@
 
 let roster = [
   ['Number', 'Name', 'Position', 'Points per Game'],
-  [12, 'Joe Schmo', 'Center', [14, 32, 7, 0, 23] ],
-  [9, 'Ms. Buckets', 'Point Guard', [19, 0, 11, 22, 0] ],
-  [31, 'Harvey Kay', 'Shooting Guard', [0, 30, 16, 0, 25] ],
-  [7, 'Sally Talls', 'Power Forward ', [18, 29, 26, 31, 19] ],
-  [22, 'MK DiBoux ', 'Small Forward ', [11, 0, 23, 17, 0] ]
+  [12, 'Joe Schmo', 'Center', [14, 32, 7, 0, 23]],
+  [9, 'Ms. Buckets', 'Point Guard', [19, 0, 11, 22, 0]],
+  [31, 'Harvey Kay', 'Shooting Guard', [0, 30, 16, 0, 25]],
+  [7, 'Sally Talls', 'Power Forward ', [18, 29, 26, 31, 19]],
+  [22, 'MK DiBoux ', 'Small Forward ', [11, 0, 23, 17, 0]]
 ]
 
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
-function convert_roster_format (nestedArray) {
+function convert_roster_format(roster) {
   // your convert code here
+  let newFormat = []
+  for (let i = 1; i < roster.length; i++) {
+    newFormat.push(
+      {
+        [roster[0][0]]: roster[i][0],
+        [roster[0][1]]: roster[i][1],
+        [roster[0][2]]: roster[i][2],
+        [roster[0][3]]: roster[i][3]
+      }
+    )
+  }
+  return newFormat
 }
 
 let object_roster = convert_roster_format(roster)
