98c8d677dbca31e20a3094474fd9e8cc4560deae
imanuel vicky <imanuvi@imanuels-MBP.localdomain>
2018-10-31 14:05:34 +0700

    done

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -9,13 +9,24 @@ let roster = [
   [22, 'MK DiBoux ', 'Small Forward ', [11, 0, 23, 17, 0] ]
 ]
 
+
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
 function convert_roster_format (nestedArray) {
-  // your convert code here
+    let output = []
+    for (let i = 1; i < nestedArray.length; i++) {
+        output.push( {
+          [nestedArray[0][0]]: nestedArray[i][0],
+          [nestedArray[0][1]]: nestedArray[i][1],
+          [nestedArray[0][2]]: nestedArray[i][2],
+          [nestedArray[0][3]]: nestedArray[i][3]
+        } )
+    }
+    return output
 }
 
 let object_roster = convert_roster_format(roster)
+
 console.log(object_roster[2])
 
 // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
