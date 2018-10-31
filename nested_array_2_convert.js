54f6127020f91324e1ea73707b5feb54f35b8ef4
taqiabdulaziz <svx.annexiv@gmail.com>
2018-10-31 16:47:25 +0700

    fix nested array 2 git push origin taqiabdulaziz
    exit
    c
    C

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -6,14 +6,16 @@ let roster = [
   [9, 'Ms. Buckets', 'Point Guard', [19, 0, 11, 22, 0] ],
   [31, 'Harvey Kay', 'Shooting Guard', [0, 30, 16, 0, 25] ],
   [7, 'Sally Talls', 'Power Forward ', [18, 29, 26, 31, 19] ],
-  [22, 'MK DiBoux ', 'Small Forward ', [11, 0, 23, 17, 0] ]
+  [22, 'MK DiBoux ', 'Small Forward ', [11, 0, 23, 17, 0] ],
+
+  
 ]
 
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
 function convert_roster_format (nestedArray) {
-  var JSON = []
-  var obj = {}
+  let JSON = []
+  let obj = {}
 
   for (let i = 1; i < nestedArray.length; i++) {
       obj[nestedArray[0][0]] = nestedArray[i][0]
@@ -31,8 +33,9 @@ let object_roster = convert_roster_format(roster)
 console.log(object_roster[2])
 
 // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
+console.log(convert_roster_format(roster));
 
-console.log(object_roster[0]['Name'] == 'Joe Schmo') // outputs true
+// console.log(object_roster[0]['Name'] == 'Joe Schmo') // outputs true
 
 module.exports = {
   convert_roster_format
